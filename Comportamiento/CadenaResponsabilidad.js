0b7361b53134c9e70de6f7bfb80572c2784b2e60
Miguel_Parra <pmjm0990@gmail.com>
2021-10-20 09:02:10 -0500

    Ejemplo del patron de comportamiento cadena de responsabilidad en JS

---
+++ Comportamiento/CadenaResponsabilidad.js
@@ -0,0 +1,16 @@
+class Suma {
+    constructor(v = 0) {
+        this.val = v
+    }
+    suma(v) {
+        this.val += v
+        return this
+    }
+}
+
+const valor = new Suma(1)
+valor.suma(1)
+    .suma(2)
+    .suma(3)
+    .suma(4)
+    .val
